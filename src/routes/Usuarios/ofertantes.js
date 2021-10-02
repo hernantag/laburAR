28727763f672e94d8c1c808727772eaacd419710
TAG <hernan.tag@hotmail.com>
2021-10-01 21:44:34 -0300

    filtering

--- src/routes/Usuarios/ofertantes.js
+++ src/routes/Usuarios/ofertantes.js
@@ -33,7 +33,6 @@ router.get('/Ofertante/Empleo/:page', loggedIn, isOfertante, async (req,res,next
           let pagina = (req.params.page)
           let total = await pool.query("SELECT * FROM solicitud");
           total = total.length
-          console.log(pagina)
 
           res.render('empleoOfer.ejs',{
             solicitudes,nombre,pagina,total
@@ -44,6 +43,63 @@ router.get('/Ofertante/Empleo/:page', loggedIn, isOfertante, async (req,res,next
           })
 
 
+          router.post("/Ofertante/Empleo/Filter",loggedIn, isOfertante, async (req, res) => {
+
+            
+            let filtros = req.body;
+            console.log(filtros)
+           
+          /*   let solicitud = await pool.query("SELECT * FROM vehiculo WHERE ID_Solicitud IS NOT NULL") */
+            
+
+            let consulta = 'SELECT * FROM solicitud WHERE '
+            console.log(consulta)
+            comilla = '"'
+            let counterAND = 0
+
+            //si viene vacio
+          if (filtros.Rubro === 'vacio' && filtros.SubRubro === 'vacio' && filtros.Experiencia === 'vacio' && filtros.Fecha === 'vacio'){
+            
+            res.redirect('/Ofertante/Empleo/')
+            
+          }
+            else {
+            //si viene con filtros
+              if (filtros.Rubro != 'vacio'){
+                  consulta = consulta + ' Rubro = ' + comilla + filtros.Rubro + comilla
+                  console.log(consulta)
+                  counterAND = (counterAND)-(-1)
+              }
+                //para agregar and
+              if (filtros.Fecha != 'vacio'){
+                
+                        if (counterAND != 0){
+                          consulta = consulta + ' AND Descripcion_L = ' + comilla + filtros.Fecha + comilla
+                          console.log(consulta)
+                        }else{
+                          consulta = consulta + ' Descripcion_L = ' + comilla + filtros.Fecha + comilla
+                          console.log(consulta)
+                        }
+                        counterAND = (counterAND)-(-1)
+              }
+                  
+                  
+              res.redirect('/Ofertante/Empleo/')
+    
+ 
+            }})
+
+          
+            
+          
+
+          /* let AND = "AND ID_SubRubro"
+          console.log(AND)
+          let solicitudes = await pool.query("SELECT * FROM solicitud WHERE ID_Solicitud IS NOT NULL ?" ,[AND +filtros.Rubro])
+          let total = solicitudes.length    
+                       
+          let nombre = req.user.nombre
+          let pagina = (req.params.page) */
 
 /* router.get("/cliente", async (req, res) => {
     let mensaje = req.flash("mensaje");
